30294c56da6a870612d416c9e43ecf7b1eb7ea73
Coreylsnodgrass <coreylsnodgrass@gmail.com>
2024-04-14 03:52:09 -0400

    First upload to git.

--- Final_Project_ServerSide/Controllers/TasksController.cs
+++ Final_Project_ServerSide/Controllers/TasksController.cs
@@ -1,6 +1,40 @@
-﻿namespace Final_Project_ServerSide.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Final_Project_ServerSide.Controllers
 {
-    public class TasksController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
     {
+        private static List<string> tasks = new List<string>(); // Mock database for tasks
+
+        // GET: api/Tasks
+        [HttpGet]
+        public IActionResult GetTasks()
+        {
+            return Ok(tasks);
+        }
+
+        // POST: api/Tasks
+        [HttpPost]
+        public IActionResult PostTask(string taskName)
+        {
+            tasks.Add(taskName);
+            return CreatedAtAction(nameof(PostTask), new { taskName = taskName });
+        }
+
+        // DELETE: api/Tasks/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTask(int id)
+        {
+            if (id < 0 || id >= tasks.Count)
+            {
+                return NotFound();
+            }
+
+            tasks.RemoveAt(id);
+            return NoContent();
+        }
     }
 }
---
+++ To-Dooey-Console/To-Dooey-Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using (var client = new HttpClient())
+        {
+            string baseUrl = "http://localhost:5138/api/";
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(baseUrl + "tasks"); // Make sure "tasks" is the correct endpoint
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+    }
+}
